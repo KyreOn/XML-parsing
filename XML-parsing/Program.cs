a7af7475f72b3d2275caee6212138887501b4c25
KyreOn <kireonbk@gmail.com>
2024-09-28 22:48:21 +0500

    Join with object levels

--- XML-parsing/Program.cs
+++ XML-parsing/Program.cs
@@ -9,7 +9,10 @@ namespace XML_parsing;
 internal static class Program
 {
     private static DateOnly _date;
-    private static XDocument _fullXml = new XDocument();
+    private static XDocument _objectXml = new XDocument();
+    private static XDocument _levelXml = new XDocument();
+    private static string[] _objectLevels = Array.Empty<string>();
+    private static IEnumerable<IGrouping<string, string[]>>? _finalResults;
     
     private static readonly HttpClient HttpClient = new();
     private static async Task Main(string[] args)
@@ -18,8 +21,15 @@ internal static class Program
         ReadZip();
         File.Delete("data.zip");
         File.Delete("version.txt");
-        _fullXml.Save("full.xml");
-        Query();
+        _objectXml.Save("full.xml");
+        _finalResults = Query();
+        foreach (var group in _finalResults)
+        {
+            foreach (var item in group)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
     }
 
     private static async Task GetFile()
@@ -43,7 +53,13 @@ internal static class Program
                 ParseDate(reader.ReadLine());
             }
 
-            if (entry.Name.StartsWith($"AS_ADDR_OBJ_2"))
+            if (entry.Name.StartsWith("AS_OBJECT_LEVELS"))
+            {
+                using var stream = entry.Open();
+                GetObjectLevels(stream);
+            }
+            //TODO сделать условие более универсальным
+            if (entry.Name.StartsWith("AS_ADDR_OBJ_2"))
             {
                 using var stream = entry.Open();
                 AddXml(stream);
@@ -57,28 +73,39 @@ internal static class Program
         _date = new DateOnly(int.Parse(dateParts[0]), int.Parse(dateParts[1]), int.Parse(dateParts[2]));
     }
 
+    private static void GetObjectLevels(Stream xmlStream)
+    {
+        var levelXml =  XDocument.Load(xmlStream);
+        _levelXml = levelXml;
+        var result = levelXml.Descendants("OBJECTLEVEL").Select(x => x.Attribute("NAME").Value).ToArray();
+        _objectLevels = result;
+    }
+
     private static void AddXml(Stream xmlStream)
     {
         var xDoc = XDocument.Load(xmlStream);
-        if (!_fullXml.Descendants().Any())
-            _fullXml = xDoc;
+        if (!_objectXml.Descendants().Any())
+            _objectXml = xDoc;
         else
-            _fullXml.Root?.Add(xDoc.Descendants("OBJECT"));
+            _objectXml.Root?.Add(xDoc.Descendants("OBJECT"));
     }
 
-    private static void Query()
+    private static IEnumerable<IGrouping<string, string[]>> Query()
     {
-        var result = from el in _fullXml.Descendants("OBJECT")
-            where el.Attribute("ISACTIVE")?.Value == "1"
-            select new [] {el.Attribute("NAME").Value, el.Attribute("TYPENAME").Value, el.Attribute("LEVEL").Value};
-        var groupedResult = result.OrderBy(x => x[0]).GroupBy(x => x[2]);
-        foreach (var group in groupedResult)
-        {
-            Console.WriteLine("-----------------------------------");
-            foreach (var element in group)
-            {
-                Console.WriteLine(string.Join(" ", element));
-            }
-        }
+        var groupedResult = _objectXml.Descendants("OBJECT").Join(_levelXml.Descendants("OBJECTLEVEL"), x => x.Attribute("LEVEL").Value, y => y.Attribute("LEVEL").Value, (x, y)
+                => new
+                {
+                    IsActive = x.Attribute("ISACTIVE"), Name = x.Attribute("NAME"), TypeName = x.Attribute("TYPENAME"),
+                    Level = x.Attribute("LEVEL"), LevelName = y.Attribute("NAME")
+                })
+            .Where(x => x.IsActive.Value == "1")
+            .Select(x => new [] {x.Name.Value, x.TypeName.Value, x.Level.Value, x.LevelName.Value})
+            .OrderBy(x => x[0]).GroupBy(x => x[2]);
+        return groupedResult;
+    }
+
+    private static void CreateDocument()
+    {
+        
     }
 }
