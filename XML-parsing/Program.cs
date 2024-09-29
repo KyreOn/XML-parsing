9f28f0cee8f22e5adabefc54a7de4093605d9ba0
KyreOn <kireonbk@gmail.com>
2024-09-29 17:32:27 +0500

    Refactoring and commenting

--- XML-parsing/Program.cs
+++ XML-parsing/Program.cs
@@ -7,50 +7,41 @@ using System.Xml;
 using System.Xml.Linq;
 
 namespace XML_parsing;
-//TODO рефакторинг
+
 internal static class Program
 {
-    //TODO убрать лишние поля
-    private static DateOnly _date;
-    private static XDocument _objectXml = new XDocument();
-    private static XDocument _levelXml = new XDocument();
-    private static string[] _objectLevels = Array.Empty<string>();
-    private static IEnumerable<IGrouping<string, string[]>>? _finalResults;
-    
+    private static string _date = "";
+    private static XDocument _objectXml = new();
+    private static XDocument _levelXml = new();
+    private static IEnumerable<IGrouping<string, string[]>> _finalResults = new List<IGrouping<string, string[]>>();
     private static readonly HttpClient HttpClient = new();
-    private static async Task Main(string[] args)
+    
+    private static async Task Main()
     {
         await GetFile();
         ReadZip();
-        File.Delete("data.zip");
-        File.Delete("version.txt");
-        _objectXml.Save("full.xml");
-        _finalResults = Query();
-        foreach (var group in _finalResults)
-        {
-            foreach (var item in group)
-            {
-                Console.WriteLine(string.Join(" ", item));
-            }
-        }
+        Query();
         CreateDocument();
         var p = new Process();
-        p.StartInfo = new ProcessStartInfo("index.html")
+        p.StartInfo = new ProcessStartInfo("report.html")
         { 
             UseShellExecute = true 
         };
         p.Start();
-    }
-
+        CleanUp();
+    }   
+    
+    //Получение ZIP-архива
     private static async Task GetFile()
     {
         var response = await HttpClient.GetAsync("http://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo");
         var jsonResponse = JsonNode.Parse(await response.Content.ReadAsStringAsync());
-        using var s = HttpClient.GetStreamAsync(jsonResponse?["GarXMLDeltaURL"]?.ToString());
+        using var s = HttpClient.GetStreamAsync(jsonResponse["GarXMLDeltaURL"].ToString());
         await using var fs = new FileStream("data.zip", FileMode.OpenOrCreate);
         await s.Result.CopyToAsync(fs);
     }
-
+  
+    //Обработка содержимого ZIP-архива
     private static void ReadZip()
     {
         using var zipFile = ZipFile.OpenRead("data.zip");
@@ -68,7 +59,6 @@ internal static class Program
                 using var stream = entry.Open();
                 GetObjectLevels(stream);
             }
-            //TODO сделать условие более универсальным
             if (entry.Name.StartsWith("AS_ADDR_OBJ_2"))
             {
                 using var stream = entry.Open();
@@ -77,30 +67,32 @@ internal static class Program
         }
     }
 
-    private static void ParseDate(string? date)
+    //Приведение даты к необходимому формату
+    private static void ParseDate(string date)
     {
-        var dateParts = date!.Split('.');
-        _date = new DateOnly(int.Parse(dateParts[0]), int.Parse(dateParts[1]), int.Parse(dateParts[2]));
+        var dateParts = date.Split('.');
+        _date = $"{dateParts[2]}.{dateParts[1]}.{dateParts[0]}";
     }
-
+    
+    //Получение списка уровней адресных объектов
     private static void GetObjectLevels(Stream xmlStream)
     {
         var levelXml =  XDocument.Load(xmlStream);
         _levelXml = levelXml;
-        var result = levelXml.Descendants("OBJECTLEVEL").Select(x => x.Attribute("NAME").Value).ToArray();
-        _objectLevels = result;
     }
-
+    
+    //Объединение XML-документов
     private static void AddXml(Stream xmlStream)
     {
         var xDoc = XDocument.Load(xmlStream);
         if (!_objectXml.Descendants().Any())
             _objectXml = xDoc;
         else
-            _objectXml.Root?.Add(xDoc.Descendants("OBJECT"));
+            _objectXml.Root.Add(xDoc.Descendants("OBJECT"));
     }
-
-    private static IEnumerable<IGrouping<string, string[]>> Query()
+    
+    //Отбор полученных данных
+    private static void Query()
     {
         var groupedResult = _objectXml.Descendants("OBJECT")
             .Join(_levelXml.Descendants("OBJECTLEVEL"), x => x.Attribute("LEVEL").Value,
@@ -111,40 +103,45 @@ internal static class Program
                 })
             .Where(x => x.IsActive.Value == "1")
             .Select(x => new [] {x.Name.Value, x.TypeName.Value, x.Level.Value, x.LevelName.Value})
-            .OrderBy(x => x[0]).OrderBy(x => x[2]).GroupBy(x => x[3]);
-        return groupedResult;
+            .OrderBy(x => x[0]).ThenBy(x => x[2]).GroupBy(x => x[3]);
+        _finalResults = groupedResult;
     }
-
+    
+    //Формирование HTML-документа на основе отобранных данных
     private static void CreateDocument()
     {
-        var sb = new StringBuilder();
+        var html = new StringBuilder();
         
-        sb.AppendLine("<head>");
-        sb.AppendLine("<link rel=\"stylesheet\" href=\"index.css\">");
-        sb.AppendLine("</head>");
-        sb.AppendLine("<body>");
-        //TODO поправить формат даты
-        sb.AppendLine($"<h3>Отчет по добавленным адресным объектам за {_date.Day}.{_date.Month}.{_date.Year}</h3>");
-        sb.AppendLine("<hr>");
-        foreach (var group in _finalResults)
+        html.AppendLine("<head>");
+        html.AppendLine("<link rel=\"stylesheet\" href=\"report.css\">");
+        html.AppendLine("</head>");
+        html.AppendLine("<body>");
+        html.AppendLine($"<h3>Отчет по добавленным адресным объектам за {_date}</h3>");
+        html.AppendLine("<hr>");
+        foreach (var group in _finalResults!)
         {
-            sb.AppendLine("<Table>");
-            sb.AppendLine($"<Caption>{group.Key}</Caption>");
-            sb.AppendLine("<TR>");
-            sb.AppendLine("<TH>Тип объекта</TH>");
-            sb.AppendLine("<TH>Наименование</TH>");
-            sb.AppendLine("</TR>");
+            html.AppendLine("<Table>");
+            html.AppendLine($"<Caption>{group.Key}</Caption>");
+            html.AppendLine("<TR>");
+            html.AppendLine("<TH>Тип объекта</TH>");
+            html.AppendLine("<TH>Наименование</TH>");
+            html.AppendLine("</TR>");
             foreach (var element in group)
             {
-                sb.AppendLine("<TR>");
-                sb.AppendLine($"<TD>{element[1]}</TD>");
-                sb.AppendLine($"<TD>{element[0]}</TD>");
-                sb.AppendLine("</TR>");
+                html.AppendLine("<TR>");
+                html.AppendLine($"<TD>{element[1]}</TD>");
+                html.AppendLine($"<TD>{element[0]}</TD>");
+                html.AppendLine("</TR>");
             }
-            sb.AppendLine("</Table>");
+            html.AppendLine("</Table>");
         }
-        sb.AppendLine("</body>");
-        var s = sb.ToString();
-        File.WriteAllText("index.html", s);
+        html.AppendLine("</body>");
+        File.WriteAllText("report.html", html.ToString());
+    }
+    
+    //Удаление ненужных файлов
+    private static void CleanUp()
+    {
+        File.Delete("data.zip");
     }
 }
